7e0cc59a4b3acee6a3fa5f6d8844412e338bf505
Matthew Sheppard <matthew.sheppard@23065-MBPR.local>
2018-01-18 11:27:02 +0000

    Pusing kops pre-reqs

---
+++ prereqs/kops_pre_reqs.tf
@@ -0,0 +1,79 @@
+# Terraform configuration file to create
+# the pre-requisites for kops on AWS
+
+resource "aws_iam_user" "kops" {
+	name				= "kops"
+}
+
+resource "aws_iam_access_key" "kops" {
+	user				= "${aws_iam_user.kops.name}"
+}
+
+resource "aws_iam_user_policy_attachment" "kops_mgd_pol_1" {
+	user				= "${aws_iam_user.kops.name}"
+	policy_arn	= "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "kops_mgd_pol_2" {
+	user        = "${aws_iam_user.kops.name}"
+	policy_arn	= "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "kops_mgd_pol_3" {
+	user				= "${aws_iam_user.kops.name}"
+	policy_arn  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "kops_mgd_pol_4" {
+	user        = "${aws_iam_user.kops.name}"
+	policy_arn  = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "kops_mgd_pol_5" {
+	user        = "${aws_iam_user.kops.name}"
+	policy_arn  = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_s3_bucket" "kops_config_bucket" {
+	bucket			= "{my_bucket_name}"
+	acl					=	 "private"
+
+	versioning {
+		enabled = true
+	}
+
+	tags {
+		Name    = "kops Cluster Configuration Bucket"
+	}
+}
+
+resource "aws_s3_bucket_policy" "b" {
+	bucket			= "${aws_s3_bucket.kops_config_bucket.id}"
+	policy			=<<POLICY
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+				"Sid": "Allow kops user access to k8s cluster configuration bucket",
+				"Effect": "Allow",
+				"Principal": {
+						"AWS": [
+								"${aws_iam_user.kops.arn}"
+						]
+				},
+				"Action": [
+						"s3:GetBucketLocation",
+						"s3:ListBucket",
+						"s3:GetObject",
+						"s3:PutObject"
+				],
+				"Resource": [
+						"${aws_s3_bucket.kops_config_bucket.arn}",
+						"${aws_s3_bucket.kops_config_bucket.arn}/*"
+				]
+		}
+	]
+}
+POLICY
+
+}
