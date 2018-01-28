c2451b559b48ce0639e1375a0c62e60dcd78eb1c
Matthew Sheppard <matthew.sheppard@23065-MBPR.local>
2018-01-28 15:02:32 +0000

    Fixing merge

--- external_dns/main.tf
+++ external_dns/main.tf
@@ -1,10 +1,6 @@
 provider "aws" {
   region      = "eu-west-1"
-<<<<<<< HEAD
   profile     = "slalom"
-=======
-  profile     = "default"
->>>>>>> 690901a2b0356dfdae2376b0167b380ea62f5317
 }
 
 terraform {
@@ -12,10 +8,6 @@ terraform {
     bucket = "slalom-k8s-terraform"
     key    = "external_dns/terraform.tfstate"
     region = "eu-west-1"
-<<<<<<< HEAD
     profile = "slalom"
-=======
-    profile = "default"
->>>>>>> 690901a2b0356dfdae2376b0167b380ea62f5317
   }
 }
