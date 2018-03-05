bb45678cc49244ec8e0b86fd72f9579bd2719647
Donald Carnegie <donald.carnegie@slalom.com>
2018-03-05 13:15:54 +0000

    RBAC change to deploy script

--- build-scripts/deploy-k8s.sh
+++ build-scripts/deploy-k8s.sh
@@ -3,6 +3,12 @@ GREEN='\033[0;32m'
 NC='\033[0;0m'
 export PATH=$PATH:$(pwd)
 
+echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
+cd ../rbac/
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+echo -e "${GREEN}==== Done deploying iam role ====${NC}"
+echo ''
+
 echo -e "${GREEN}==== Deploying iam role ====${NC}"
 cd ../kube2iam/
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
