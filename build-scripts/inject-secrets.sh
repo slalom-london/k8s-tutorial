3fa3738c6bf9697f555bffb8700d99349fd39580
Donald Carnegie <donald.carnegie@slalom.com>
2018-03-05 13:11:13 +0000

    Make inject-secrets.sh generic

--- build-scripts/inject-secrets.sh
+++ build-scripts/inject-secrets.sh
@@ -3,7 +3,7 @@ mkdir ~/.kube
 mv ./build-scripts/kubeconfig ~/.kube/config
 
 #decrypt the large secrets
-openssl aes-256-cbc -K $encrypted_e6568868d258_key -iv $encrypted_e6568868d258_iv -in ./build-scripts/large-secrets.txt.enc -out large-secrets.txt -d
+{Your openssl command from the encrypt secrets stage here}
 
 # run the script to get the secrets as environment variables
 source ./large-secrets.txt
@@ -11,11 +11,11 @@ export $(cut -d= -f1 ./large-secrets.txt)
 
 
 # Set kubernetes secrets
-./kubectl config set clusters.cluster.zigzag-london.com.certificate-authority-data "$CERTIFICATE_AUTHORITY_DATA"
-./kubectl config set users.cluster.zigzag-london.com.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
-./kubectl config set users.cluster.zigzag-london.com.client-key-data "$CLIENT_KEY_DATA"
-./kubectl config set users.cluster.zigzag-london.com.password "$KUBE_PASSWORD"
-./kubectl config set users.cluster.zigzag-london.com-basic-auth.password "$KUBE_PASSWORD"
+./kubectl config set {Your cluster url here}.certificate-authority-data "$CERTIFICATE_AUTHORITY_DATA"
+./kubectl config set {Your cluster url here}.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
+./kubectl config set {Your cluster url here}.client-key-data "$CLIENT_KEY_DATA"
+./kubectl config set {Your cluster url here}.password "$KUBE_PASSWORD"
+./kubectl config set {Your cluster url here}-basic-auth.password "$KUBE_PASSWORD"
 
 # set AWS secrets
 mkdir ~/.aws
