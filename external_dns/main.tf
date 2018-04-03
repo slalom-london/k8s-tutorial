af50e11c7f91f7537583fe2beda838a74eb8f55e
Matthew Sheppard <matthew.sheppard@slalom.com>
2018-04-03 15:08:35 +0100

    Minor updates following review of tutorial

--- external_dns/main.tf
+++ external_dns/main.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region      = "eu-west-1"
   profile     = "default"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "{your-tf-s3-bucket}"
-    key    = "external_dns/terraform.tfstate"
-    region = "eu-west-1"
-    profile = "kops"
-  }
-}
--- ingress/main.tf
+++ ingress/main.tf
@@ -2,12 +2,3 @@ provider "aws" {
   region      = "eu-west-1"
   profile     = "default"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "slalom-k8s-terraform"
-    key    = "ingress/terraform.tfstate"
-    region = "eu-west-1"
-    profile = "default"
-  }
-}
